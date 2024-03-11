c849473aa5f35e7036de732cf15392a0aa9c1c1b
7044lacaba <johnlacaba6@gmail.com>
2024-03-11 15:55:45 -0700

    index/layout

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from flask import Flask, redirect, render_template, request, session
 
 
-pass
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
