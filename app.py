c76e088c67b5ca26e09eeedf2d6a698069b405c6
7044lacaba <johnlacaba6@gmail.com>
2024-03-11 17:44:21 -0700

    login/logout

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template, request, session
-
+from flask_session import Session
 
 app = Flask(__name__)
 
@@ -11,5 +11,16 @@ Session(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", name=session.get("name"))
 
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        session["name"] = request.form.get("name")
+        return redirect("/")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
